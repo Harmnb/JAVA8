7e3419d2fa550eada0c4803bd9a77cb7fd686ef4
Harmnb <109940533+Harmnb@users.noreply.github.com>
2022-07-26 12:01:21 +0530

    Assignment no.1

---
+++ harsh.java
@@ -0,0 +1,60 @@
+//Q5
+//WRAPPER CLASS: Wrapper class are those class which use primitive data types such as int, float ,boolean,short,long,byte,char etc. It allows us to use primitive data types as objects.
+//for example: Integer i = 20;
+//it mainly uses two concepts i.e boxing and unboxing.
+
+
+//Q2
+//Class name= class Demo,class Test
+//function name= myFunction(), myFunction2()
+//variable=myFunction,myFunction2
+//static variable= Static myFunction1,Static myFunction
+
+//Q3
+//1part:- .Class file:- When we write program in java and with  help of compiler we compile the program then compiler generates the byte code which is of the form .class . Then this code can be interpreted at any system.
+ //       .java file :- It may be defined as file that can be compiled by compiler then give class file.
+ //2part:- Java is not complete OOP's programming language because in JAVA there is primitive data types like byte,short,int,long,float,double,boolean,char that are not used as objects.Therefore java is not compete oop's programmimg, but it can be used as object oriented programming.
+ //3part:- NR-WA
+ //int void (int a,int b){
+	 //int sum = a+b:
+	 //System.out.println("sum");
+	 
+	 
+//Q1
+//class Demo{
+	//public static void main (String args[]){
+		//int age = 20;
+		//System.out.println("my age is 20");
+		//System.out.println("my name is harsh");
+		//Demo obj = new Demo();
+		//obj.hobby();
+	//	}
+	//void hobby(){
+//System.out.println(" my hobby is drawing");
+//}
+//}
+
+//Q4
+//class CalciWorking{
+//	public static void main (String args[]){
+		//addy();
+		//subby();
+		//CalciWorking obj = new CalciWorking();
+		//obj.diviji();
+		//obj.mull();
+//}
+//}
+//class Calci{
+	//static void addy(){
+		//System.out.println("addy");
+	//}
+	//static void subby(){
+//System.out,println("subby");
+	//}
+	//void diviji(){
+		//System.out.println("diviji");
+	//}
+	//void mull(){
+		//System.out.println("mull");
+	//}
+//}
