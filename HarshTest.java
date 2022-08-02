43c5c49861d931befacd0d2e0cd9cb443975daf1
Harmnb <109940533+Harmnb@users.noreply.github.com>
2022-08-02 12:16:54 +0530

    assignment 2

---
+++ HarshTest.java
@@ -0,0 +1,175 @@
+//ANS Q1
+//import java.util.Scanner;
+//class War{
+//public static void main(String args[])
+//{
+  //Scanner obj = new Scanner(System.in);
+  //System.out.println("Enter the no");
+  //int a = obj.nextInt();
+  //if(a%2==0)
+  //{
+    //System.out.println("no is even");
+	//}
+	//else
+	//{
+	  //System.out.println("no is odd");
+	 //}
+	 //}
+	 //}
+//ANS Q2
+//import java.util.Scanner;
+//class War{
+	//public static void main(String args[])
+	//{
+      //Scanner obj = new Scanner(System.in);
+      //System.out.println ("Enter the no");
+      //int a = obj.nextInt();
+     // if(a>55)
+	  //{
+		  //System.out.println("No is greater than 55");
+	 // }
+	 // else
+	 // {
+		  
+		 // System.out.println("No is les than 55");
+	  //}
+	//}
+//}
+  //ANS Q6
+  //import java.util.Scanner;
+ // class War{
+	 // public static void main(String args[])
+	 // {
+		// int i,no;
+		//  Scanner obj = new Scanner(System.in);
+		 // System.out.println("Enter a number");
+		  //int i = obj.nextInt();
+		  //for(i=1;i<=no;i++)
+		  //{
+			  //System.out.println(i);
+			  
+			 // }
+		 // }
+	 // }
+ 
+		  
+	//ANS Q4	  
+//import java.util.Scanner;
+//class War{
+	//public static void main(String args[])
+	//{
+		//Scanner obj = new Scanner(System.in);
+		//int rollNo;
+		//rollNo = obj.nextInt();
+		//switch(rollNo)
+		//{
+			//case 579:
+			//System.out.println("Sonu roll no");
+			//break;
+			//case 546:
+			//System.out.println("Monu roll no");
+			//break;
+			//case 553:
+			//System.out.println("Manu roll no");
+			//break;
+			//case 544:
+			//System.out.println("Meenu roll no");
+			//break;
+			//case 516:
+			//System.out.println("Meena roll no");
+			//break;
+			//default:
+			//System.out.println("invalid");
+			//break;
+			
+		//}
+	//}
+//}
+			
+			
+//ANS Q3
+  // import java.util.Scanner;
+//class War
+//{
+ // public static void main(String args[])
+  //{
+   // Scanner obj = new Scanner(System.in);
+     //System.out.println("Enter no");
+     //int a = obj.nextInt();
+     //int b = obj.nextInt();
+     //if(a==b)
+	 //{
+      // System.out.println("no is equal");
+
+	// }
+    // else if(a<b){
+		// System.out.println("no is greater");
+	 //}
+	// else{
+		// System.out.println("not equal");
+	// }
+ // }
+//}
+// ANS Q5
+// PASS BY VALUE: java only support pass by value .It do not support pas by reference . Calling a method by passing a value is known as pass by value.for example if we have notes of one subject and others have its copy so if we want to do any change then changes will take place in copy but not in original notes.
+ 
+//Pass by Reference : java donot support pass by reference.,because in pass by reference pointers are used to point the address but java donot involves the use of pointers.
+
+//ANS Q8
+//class War {
+	//public static void main(String args[])
+	//{
+		
+		//int a = 3;
+		//int b = 5;
+		//for(int i=1;i<=a;i++){
+			//for(int j=i;j<=b;j++){
+				//if(i%2==0&&j%2==0||i%2==1&&j%2==1)
+				//{
+					//System.out.print("1");
+				//}
+				//else{
+					//System.out.println("0");
+				//} 
+				//System.out.println("");
+			//}
+		//}
+	//}
+				
+					
+
+
+		 
+		 
+			
+			
+			
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		  
+		  
+		  
+		  
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
